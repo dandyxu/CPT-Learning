43bf6b6b23f220fe7ed8ab2dc891b18c7a5bb2bb
dandyxu <dandyjefferson@gmail.com>
2017-10-27 15:07:27 +0100

    Initial: WP Setup and Simple Custom Post Type Plugin Created

---
+++ wp-content/plugins/posttypes.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: Dandy's Custom Post Type and Taxonomies
+ * Description: A simple plugin to add Custom Post Type and Taxonomies
+ * Version: 1.0
+ * Author: Dandy Xu
+ * License: GPL2
+ *
+ */
+
+
