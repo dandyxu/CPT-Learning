47c4f8398853b308afbcc9058dda7a74d9a52b3d
dandyxu <dandyjefferson@gmail.com>
2017-11-05 12:24:45 +0000

    CPT Challenge: CPT for Reviews with more supports and taxomonies

--- wp-content/plugins/posttypes.php
+++ wp-content/plugins/posttypes.php
@@ -9,6 +9,8 @@
  */
 
 function my_custom_posttypes() {
+
+	// CPT for Jobs
 	$labels = array(
 		'name'              => 'Jobs',
 		'singular_name'     => 'Jobs',
@@ -43,7 +45,44 @@ function my_custom_posttypes() {
 		'show_in_rest'			=> true,
 		'rest_base' 			=> 'jobs-api'
 	);
- 	register_post_type( 'jobs', $args );
+	 register_post_type( 'jobs', $args );
+	 
+	// CPT for Reviews
+	 $labels = array(
+		'name'              => 'Reviews',
+		'singular_name'     => 'Review',
+		'menu_name'         => 'Reviews',
+		'name_admin_bar'    => 'Reviews',
+		'add_new'           => 'Add New',
+		'add_new_item'      => 'Add New Review',
+		'new_item'          => 'New Review',
+		'edit_item'         => 'Edit Review',
+		'view_item'         => 'View Review',
+		'all_items'         => 'All Reviews',
+		'search_items'      => 'Search Reviews',
+		'parent_item_colon' => 'Parent Reviews:',
+		'not_found'         => 'No reviews found.',
+		'not_found_in_trash'=> 'No reviews found in Trash.',
+	);
+
+	$args = array(
+		'labels'                => $labels,
+		'public'                => true,
+		'publicly_queryable'    => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'menu_icon'             => 'dashicons-universal-access-alt',
+		'query_var'             => true,
+		'rewrite'               => array( 'slug' => 'reviews' ),
+		'capability_type'       => 'post',
+		'has_archive'           => true,
+		'hierarchical'          => false,
+		'supports'              => array( 'title', 'editor', 'thumbnail', 'author', 'excerpt', 'comments' ),
+		'show_in_rest'			=> true,
+		'taxonomies'			=> array ( 'category', 'post_tag')
+	);
+ 	register_post_type( 'reviews', $args );
 }
 add_action( 'init', 'my_custom_posttypes' );
 
