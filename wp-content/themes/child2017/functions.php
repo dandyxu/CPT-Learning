e9e2d97eae98c36b7aef6f24987fe4e23d3af739
dandyxu <dandyjefferson@gmail.com>
2017-11-07 22:48:25 +0000

    Mixed display all posts and CPT (reviews) pre_get_posts, need to add conditional statement

---
+++ wp-content/themes/child2017/functions.php
@@ -0,0 +1,12 @@
+<?php
+
+// Mix display all posts and reviews
+function my_add_reviews( $query ) {    
+    if( !is_admin() && $query->is_main_query() ) {
+        if( $query->is_home() ) {
+            $query->set( 'post_type', array( 'post', 'reviews') );
+        }
+    }
+}
+
+add_action( 'pre_get_posts', 'my_add_reviews' );
